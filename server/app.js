1a41d7d4b412675600680f4ace5fec9d9e5df568
ZanderSparrow <sondra.silverhawk@hush.com>
2016-02-05 16:06:03 -0800

    Add tables to chat database and connect in app
    plus fix style issues in spec

--- server/app.js
+++ server/app.js
@@ -8,8 +8,31 @@ var parser = require('body-parser');
 // Router
 var router = require('./routes.js');
 
+// Database
+var mysql = require('mysql');
+
 var app = express();
 module.exports.app = app;
+
+// Connect to database
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'cats',
+  database: 'chat'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+connection.end(function(err){
+  console.log('end the connection');
+});
 
 // Set what we are listening on.
 app.set('port', 3000);
--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,85 @@ CREATE DATABASE chat;
 
 USE chat;
 
+-- ---
+-- Table 'MESSAGES'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `MESSAGES`;
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` MEDIUMTEXT NULL DEFAULT NULL,
+  `timestamp` TIMESTAMP NULL DEFAULT NULL,
+  `room_id` INTEGER NULL DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 /* Create other tables and define schemas for them here! */
 
+-- ---
+-- Globals
+-- ---
 
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'USERS'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `USERS`;
+    
+CREATE TABLE `USERS` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` CHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'ROOMS'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `ROOMS`;
+    
+CREATE TABLE `ROOMS` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` CHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `MESSAGES` ADD FOREIGN KEY (room_id) REFERENCES `ROOMS` (`id`);
+ALTER TABLE `MESSAGES` ADD FOREIGN KEY (user_id) REFERENCES `USERS` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `USERS` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `ROOMS` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `MESSAGES` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `USERS` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `ROOMS` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `MESSAGES` (`id`,`text`,`timestamp`,`room_id`,`user_id`) VALUES
+-- ('','','','','');
 
 
 /*  Execute this file from the command line by typing:
